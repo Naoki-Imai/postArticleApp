d2085a06657eb7f9dfaf56181f427dd0be54e7c1
Naoki Imai <naokinetin0110@gmail.com>
2021-04-16 22:37:12 +0900

    post,menuモデルとbefore_action定義

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  helper_method :nav
+  before_action :nav
+
+  private
+    def nav
+      @menus = Menu.all
+    end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,23 @@
 class PostsController < ApplicationController
   def index
-    
+    @posts = Post.all
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new
+    if @post.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def posts_params
+      params.require(:post).permit(:title, :link_url, :category_id, :content)
+    end
 end
