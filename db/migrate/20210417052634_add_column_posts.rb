199782b4142d8c232fe11af4ea103d6a997a522a
Naoki-Imai <naokinetin0110@gmail.com>
2021-04-24 19:08:09 +0900

    modal編集

--- db/migrate/20210417052634_add_column_posts.rb
+++ db/migrate/20210417052634_add_column_posts.rb
@@ -1,6 +1,6 @@
 class AddColumnPosts < ActiveRecord::Migration[6.1]
   def up
-    change_column :posts, :menu_id, :string
+    change_column :posts, :menu_id, :integer
     #Ex:- change_column("admin_users", "email", :string, :limit =>25)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 2021_04_17_052634) do
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "menu_id", null: false
+    t.integer "menu_id", null: false
     t.index ["menu_id"], name: "index_posts_on_menu_id"
   end
 
