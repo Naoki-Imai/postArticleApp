829774dbe3fd4662e40b16187998497f141b2a15
Naoki-Imai <naokinetin0110@gmail.com>
2021-04-24 18:23:21 +0900

    remove migretionファイル

--- db/migrate/20210417052133_change_toposts.rb
+++
@@ -1,6 +0,0 @@
-class ChangeToposts < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :posts, :menu
-    #Ex:- change_column("admin_users", "email", :string, :limit =>25)
-  end
-end
--- db/migrate/20210417052634_add_column_posts.rb
+++ db/migrate/20210417052634_add_column_posts.rb
@@ -1,5 +1,5 @@
 class AddColumnPosts < ActiveRecord::Migration[6.1]
-  def change
+  def up
     change_column :posts, :menu_id, :string
     #Ex:- change_column("admin_users", "email", :string, :limit =>25)
   end
